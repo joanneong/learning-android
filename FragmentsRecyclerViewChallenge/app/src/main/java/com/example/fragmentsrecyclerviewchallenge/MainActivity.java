2c4dc8b4c26416e30f7f6c1d8123597ca7e5718e
joanneong <joanneongcf@u.nus.edu>
2020-06-21 23:07:35 +0800

    Add basic layout for the fragments
    Adds buttons in the action fragment to show
    the correct fragment when clicked.

--- FragmentsRecyclerViewChallenge/app/src/main/java/com/example/fragmentsrecyclerviewchallenge/MainActivity.java
+++ FragmentsRecyclerViewChallenge/app/src/main/java/com/example/fragmentsrecyclerviewchallenge/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.fragmentsrecyclerviewchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCarInfo, btnOwnerInfo;
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = this.getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .hide(fragmentManager.findFragmentById(R.id.carInfoFrag))
+                .show(fragmentManager.findFragmentById(R.id.ownerInfoFrag))
+                .commit();
+
+        btnCarInfo = findViewById(R.id.btnCarInfo);
+        btnOwnerInfo = findViewById(R.id.btnOwnerInfo);
+
+        btnCarInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .hide(fragmentManager.findFragmentById(R.id.ownerInfoFrag))
+                        .show(fragmentManager.findFragmentById(R.id.carInfoFrag))
+                        .commit();
+            }
+        });
+
+        btnOwnerInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .hide(fragmentManager.findFragmentById(R.id.carInfoFrag))
+                        .show(fragmentManager.findFragmentById(R.id.ownerInfoFrag))
+                        .commit();
+            }
+        });
+
+
     }
 }
